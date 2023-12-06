6fd810adae38119e4fb68fa2d36b14f5c1ac2ccf
Anatoly <kolganovpix@yandex.ru>
2023-12-06 17:59:27 +0500

    Добавит зациклинность Tab

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -13,7 +13,7 @@ import {
   getStatusShowModal,
 } from '../../store/products/selector';
 import ModalAddingProduct from '../modal-adding-product';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import ModalAddingSuccess from '../modal-adding-success';
 import ModalAddingReview from '../modal-adding-review';
 import { Product } from '../../types/product';
@@ -42,26 +42,61 @@ function Modal({ id }: ModalProps): JSX.Element {
   const isModalReviewSuccess = useAppSelector(getIsModalReviewSuccess);
   const isModalRemoveProduct = useAppSelector(getIsModalRemoveProduct);
   const isModalOrderSuccess = useAppSelector(getIsModalOrderSuccess);
+  const modalRef = useRef<HTMLDivElement | null>(null);
 
   const closeModal = useClosingModal;
 
   useEffect(() => {
+    let isFocus = false;
+
+    if((!isModalProduct && isModalProductSuccess) || (!isModalReview && isModalReviewSuccess)){
+      isFocus = false;
+    }
+
     if (isActive) {
-      const handleEsc = (evt: KeyboardEvent) => {
+      const handleKeyDown = (evt: KeyboardEvent) => {
         if (evt.key === 'Escape') {
           closeModal(dispatch);
+        }
+
+        if (evt.key === 'Tab') {
+          evt.preventDefault();
+
+          const childs = Array.from(modalRef.current?.querySelectorAll('a, button, input, textarea') ?? []) as [HTMLAnchorElement | HTMLButtonElement | HTMLInputElement | HTMLTextAreaElement];
+
+          for (let i = 0; i < childs.length; i++) {
+            childs[i].tabIndex = 0;
+          }
+
+          if(modalRef.current !== null && !isFocus){
+            modalRef.current.focus();
+            isFocus = true;
+            childs[0].focus();
+          }
+
+          const activeItem = document.activeElement;
+
+          for (let i = 0; i < childs.length; i++){
+            if(activeItem === childs[childs.length - 1]){
+              childs[0].focus();
+            }else if(activeItem === childs[i] && activeItem !== childs[childs.length - 1]){
+              childs[i + 1].focus();
+            }
+          }
+
         }
       };
 
       document.body.style.overflow = 'hidden';
-      window.addEventListener('keydown', handleEsc);
+      window.addEventListener('keydown', handleKeyDown);
 
       return () => {
-        window.removeEventListener('keydown', handleEsc);
+        window.removeEventListener('keydown', handleKeyDown);
         document.body.style.overflow = 'auto';
+        isFocus = false;
       };
     }
-  }, [isActive, dispatch, closeModal]);
+  }, [isActive, dispatch, closeModal, isModalProduct, isModalProductSuccess, isModalReview, isModalReviewSuccess]);
 
   useEffect(() => {
     if (statusData === Status.Success) {
@@ -77,6 +112,7 @@ function Modal({ id }: ModalProps): JSX.Element {
         'modal--narrow': isModalProductSuccess,
       })}
       data-testid="modal"
+      ref={modalRef}
     >
       <div className="modal__wrapper">
         <div className="modal__overlay" onClick={() => closeModal(dispatch)} />
